b1365ac3a941aa030894b428c6a13cb9ca14a3c3
Anthony <anthonyang86@gmail.com>
2016-11-30 21:51:58 -0500

    Original ycsb codebase is added

---
+++ run_2cli.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# usage run_benchmark.sh [redis|riak|memcached]
+for t in 12 
+do
+        echo -e "##########################    #thread: $t   #########################################"
+	for k in 1 2
+	do 
+	    ./bin/ycsb run $1 -s -P ~/benchmark/single_node_mix -P ~/benchmark/single_node_setting -p "threadcount=$t" > "results/$1/single_node/out_2cli_run_$t-$k"
+	    echo -e "###################  done: results/$1/single_node/out_run_$t-$k ###############"
+	done
+done 
+
